e0867dc1a63c4dcdcd8ccdaebfb70190f63ac292
Maksim <maksimv0202@gmail.com>
2022-10-20 22:28:32 +1000

    updated backend: added gunicorn module and updated allowed hosts in settings

--- src/backend/backend/settings.py
+++ src/backend/backend/settings.py
@@ -24,9 +24,12 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DJANGO_DEBUG']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    os.environ['DJANGO_ALLOWED_HOSTS'],
+]
 
 
 # Application definition
